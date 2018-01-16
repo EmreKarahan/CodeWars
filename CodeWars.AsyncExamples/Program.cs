eb170032a325bb57009ac3d63f7cb250f9fd6210
Emre Karahan <e.karahan@jollytur.com>
2018-01-16 12:25:50 +0300

    Async TAP Examples

---
+++ CodeWars.AsyncExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeWars.AsyncExamplesTAP
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TaskBasedAsyncPattern.CallSelamAsync();
+            //CallContinueSelamAsync();
+            Console.WriteLine("Running");
+            var a = 10 + 4;
+            Console.WriteLine(a);
+            Thread.Sleep(500);
+            Console.WriteLine("Running");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CodeWars.AsyncExamples/TaskBasedAsyncPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeWars.AsyncExamplesTAP
+{
+    public class TaskBasedAsyncPattern
+    {
+        // Async olan SelamAsync metodunu çağırıyoruz.
+        public static async void CallSelamAsync()
+        {
+            string result = await SelamAsync("Ali");
+            Console.WriteLine(result);
+        }
+
+        // SelamAsync tamamlanır tamamlanmaz çağırılacak bölümdür.
+        private static void CallContinueSelamAsync()
+        {
+            Task<string> t1 = SelamAsync("Ali");
+            t1.ContinueWith(t =>
+            {
+                string result = t.Result;
+
+                Console.WriteLine(result);
+            });
+        }
+
+        // Senkron metodumuz
+        static string Selam(string name)
+        {
+            Thread.Sleep(1000);
+            return $"Selam, {name}";
+
+        }
+
+        // Task<> task dönürecek async metodumuz
+        static Task<string> SelamAsync(string name)
+        {
+
+            return Task.Run(() => Selam(name));
+        }
+    }
+}
