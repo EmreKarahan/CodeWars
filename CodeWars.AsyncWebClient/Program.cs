533022c743e0a6a68a656685f9c19bd62a34c90b
Emre Karahan <e.karahan@jollytur.com>
2018-01-16 12:37:55 +0300

    AyncWebClient

---
+++ CodeWars.AsyncWebClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CodeWars.AsyncWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = GetWebStringAsync();
+            Console.WriteLine(data.Result);
+        }
+
+
+        internal static async Task<string> GetWebStringAsync()
+        {
+            WebClient web = new WebClient();
+            string gelen = await web.DownloadStringTaskAsync(new Uri("http://www.eksisozluk.com"));
+            return gelen;
+        }
+    }
+}
