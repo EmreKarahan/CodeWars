174e5bef6815dc18d7d05cccea0e324957611eee
Emre Karahan <emre.karahan@gmail.com>
2018-01-16 22:01:39 +0300

    Find the divisors!

---
+++ CodeWars.FindTheDivisors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.FindTheDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var result =  Kata.Divisors(12);
+        }
+    }
+
+    
+
+    public class Kata
+    {
+        public static int[] Divisors(int n)
+        {
+            List<int> l = new List<int>();
+            for (int i = 2; i <= Math.Sqrt(n); i++) if (n % i == 0) l.Add(i);
+            if (l.Count == 0) return null;
+            List<int> k = new List<int>(l.ToArray().Select(x => n / x).ToArray().Where(x => !l.Contains(x)).Reverse());
+            l.AddRange(k);
+            return l.ToArray();
+        }
+    }    
+    }    
+
+}
